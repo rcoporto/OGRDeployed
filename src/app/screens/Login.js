1110727b189c4abc2aa0da7a27021c173a2eab2f
sakurachu06 <anheresuribera@gmail.com>
2024-11-11 18:07:34 +0800

    modified input tag attributes

--- src/app/screens/Login.js
+++ src/app/screens/Login.js
@@ -226,7 +226,7 @@ function Login() {
                     </div>
                     
                     <div class="relative">
-                      <input type="password" id="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} class="block px-2.5 pb-2.5 pt-4 w-full font-lora text-sm text-bluegreen-90 bg-transparent rounded-lg border-1 border-gray-20 appearance-none focus:outline-none focus:ring-0 focus:border-bluegreen-70 peer" placeholder=" " required/>
+                      <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} class="block px-2.5 pb-2.5 pt-4 w-full font-lora text-sm text-bluegreen-90 bg-transparent rounded-lg border-1 border-gray-20 appearance-none focus:outline-none focus:ring-0 focus:border-bluegreen-70 peer" placeholder=" " required/>
                       <label htmlFor="password" class="absolute text-sm text-gray-60 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white px-2 peer-focus:px-2 peer-focus:text-bluegreen-70 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 start-1">
                       Password <span class="text-pink-50">*</span></label>
                     </div>
