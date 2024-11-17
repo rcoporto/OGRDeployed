28f5db6390d4d3ef9a1764dca5eb9b3103911c5d
davidbobis <dbbobis@up.edu.ph>
2024-11-18 00:03:37 +0800

    navbar added active link

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -142,20 +142,92 @@
 
 
 
-"use client"
-import React, { useEffect, useState } from 'react';
-import Link from 'next/link';
-import styles from './Navbar.module.css';
-import { useRouter } from 'next/navigation';
-import { useAuth } from '../context/authContext';
-import { db } from '../../../firebase/firebase';
-import { collection, query, where, getDocs } from 'firebase/firestore';
+// "use client"
+// import React, { useEffect, useState } from 'react';
+// import Link from 'next/link';
+// import styles from './Navbar.module.css';
+// import { useRouter } from 'next/navigation';
+// import { useAuth } from '../context/authContext';
+// import { db } from '../../../firebase/firebase';
+// import { collection, query, where, getDocs } from 'firebase/firestore';
+
+// const Navbar = () => {
+//   const [userName, setUserName] = useState('');
+//   const [dropdownOpen, setDropdownOpen] = useState(false); // For handling dropdown visibility
+//   const { user, logout } = useAuth();
+//   const router = useRouter();
+
+//   useEffect(() => {
+//     const fetchUserName = async () => {
+//       if (user) setUserName(user.name);
+//     };
+//     fetchUserName();
+//   }, [user]);
+
+//   const handleLogout = async () => {
+//     try {
+//       await logout();
+//       router.push('/login');
+//     } catch (error) {
+//       console.error("Error during sign-out: ", error);
+//     }
+//   };
+
+//   const toggleDropdown = () => setDropdownOpen(!dropdownOpen);
+
+//   const NAV_LINKS = [
+//     { href: '/', key: 'home', label: 'Home' },
+//     { href: '/basic-info', key: 'basic_info', label: 'Basic Information' },
+//     { href: '/families', key: 'families', label: 'Families' },
+//     { href: '/registry', key: 'registry', label: 'Registry' },
+//     { href: '/research', key: 'research', label: 'Research' },
+//     { href: '/inquire-now', key: 'inquire_now', label: 'Inquire Now' },
+//     { href: '/new-user', key: 'register_now', label: 'Register Now' },
+//     !user ? { href: '/login', key: 'login', label: 'Login' } : null
+//   ].filter(Boolean);
+
+//   return (
+//     <nav className={styles.navbar}>
+//       <div className={styles.logo}>Ocular Genetic Registry</div>
+//       <ul className={styles.navLinks}>
+//         {NAV_LINKS.map((link) => (
+//           <Link href={link.href} key={link.key} className={styles.link}>
+//             {link.label}
+//           </Link>
+//         ))}
+//       </ul>
+//       {user && (
+//         <div className={styles.userSection} onClick={toggleDropdown}>
+//           <span className={styles.userName}>Hi, {userName || 'User'}!</span>
+//           {dropdownOpen && (
+//             <div className={styles.dropdownMenu}>
+//               <button className={styles.dropdownItem} onClick={handleLogout}>
+//                 Logout
+//               </button>
+//             </div>
+//           )}
+//         </div>
+//       )}
+//     </nav>
+//   );
+// };
+
+// export default Navbar;
+
+
+"use client";
+import React, { useEffect, useState } from "react";
+import Link from "next/link";
+import styles from "./Navbar.module.css";
+import { useRouter, usePathname } from "next/navigation";
+import { useAuth } from "../context/authContext";
 
 const Navbar = () => {
-  const [userName, setUserName] = useState('');
-  const [dropdownOpen, setDropdownOpen] = useState(false); // For handling dropdown visibility
+  const [userName, setUserName] = useState("");
+  const [dropdownOpen, setDropdownOpen] = useState(false);
   const { user, logout } = useAuth();
   const router = useRouter();
+  const pathname = usePathname(); // Get the current path
 
   useEffect(() => {
     const fetchUserName = async () => {
@@ -167,7 +239,7 @@ const Navbar = () => {
   const handleLogout = async () => {
     try {
       await logout();
-      router.push('/login');
+      router.push("/login");
     } catch (error) {
       console.error("Error during sign-out: ", error);
     }
@@ -176,14 +248,14 @@ const Navbar = () => {
   const toggleDropdown = () => setDropdownOpen(!dropdownOpen);
 
   const NAV_LINKS = [
-    { href: '/', key: 'home', label: 'Home' },
-    { href: '/basic-info', key: 'basic_info', label: 'Basic Information' },
-    { href: '/families', key: 'families', label: 'Families' },
-    { href: '/registry', key: 'registry', label: 'Registry' },
-    { href: '/research', key: 'research', label: 'Research' },
-    { href: '/inquire-now', key: 'inquire_now', label: 'Inquire Now' },
-    { href: '/new-user', key: 'register_now', label: 'Register Now' },
-    !user ? { href: '/login', key: 'login', label: 'Login' } : null
+    { href: "/", key: "home", label: "Home" },
+    { href: "/basic-info", key: "basic_info", label: "Basic Information" },
+    { href: "/families", key: "families", label: "Families" },
+    { href: "/registry", key: "registry", label: "Registry" },
+    { href: "/research", key: "research", label: "Research" },
+    { href: "/inquire-now", key: "inquire_now", label: "Inquire Now" },
+    { href: "/new-user", key: "register_now", label: "Register Now" },
+    !user ? { href: "/login", key: "login", label: "Login" } : null,
   ].filter(Boolean);
 
   return (
@@ -191,14 +263,25 @@ const Navbar = () => {
       <div className={styles.logo}>Ocular Genetic Registry</div>
       <ul className={styles.navLinks}>
         {NAV_LINKS.map((link) => (
-          <Link href={link.href} key={link.key} className={styles.link}>
+          <Link
+            href={link.href}
+            key={link.key}
+            className={`${styles.link} ${
+              pathname === link.href ? styles.active : ""
+            }`} // Add active class if the current path matches the link's href
+          >
             {link.label}
           </Link>
         ))}
       </ul>
       {user && (
-        <div className={styles.userSection} onClick={toggleDropdown}>
-          <span className={styles.userName}>Hi, {userName || 'User'}!</span>
+        <div className={styles.userSection} onClick={toggleDropdown}><span
+        className={styles.userName}
+        data-fullname={userName} // For tooltip
+      >
+        Hi, {userName || 'User'}!
+      </span>
+      
           {dropdownOpen && (
             <div className={styles.dropdownMenu}>
               <button className={styles.dropdownItem} onClick={handleLogout}>
